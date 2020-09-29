b0c8606ec3ce206d7954e487a600a552f99977df
Mahbubul Haque <mhaque@darenasolutions.com>
2020-09-29 13:36:35 -0400

    Uncomment device conversion

--- src/DarenaSolutions.CCdaToFhirConverter/CCdaFhirConverter.cs
+++ src/DarenaSolutions.CCdaToFhirConverter/CCdaFhirConverter.cs
@@ -97,8 +97,8 @@ namespace DarenaSolutions.CCdaToFhirConverter
             var procedureXPath = "//n1:templateId[@root='2.16.840.1.113883.10.20.22.2.7.1']/../n1:entry/n1:procedure";
             AddConversionToBundle(procedureXPath, () => new ProcedureConverter(patientConverter.PatientId));
 
-            ////var deviceXPath = "//n1:templateId[@root='2.16.840.1.113883.10.20.22.2.23']/../n1:entry/n1:procedure";
-            ////AddConversionToBundle(deviceXPath, () => new DeviceConverter(patientConverter.PatientId));
+            var deviceXPath = "//n1:templateId[@root='2.16.840.1.113883.10.20.22.2.23']/../n1:entry/n1:procedure";
+            AddConversionToBundle(deviceXPath, () => new DeviceConverter(patientConverter.PatientId));
 
             var goalXPath = "//n1:templateId[@root='2.16.840.1.113883.10.20.22.2.60']/../n1:entry/n1:observation";
             AddConversionToBundle(goalXPath, () => new GoalConverter(patientConverter.PatientId));
