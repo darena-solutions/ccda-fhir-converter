dc502f760830394884f0850a6125d33fe3d9d58a
Mahbubul Haque <mhaque@darenasolutions.com>
2020-10-08 16:03:51 -0400

    Updated the medication statement converter. The medication statement converter is now the main entry point for medication resources. Updated medication resource to only contain the code

--- src/DarenaSolutions.CCdaToFhirConverter/MedicationStatementConverter.cs
+++ src/DarenaSolutions.CCdaToFhirConverter/MedicationStatementConverter.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Xml;
 using System.Xml.Linq;
+using System.Xml.XPath;
 using DarenaSolutions.CCdaToFhirConverter.Constants;
 using DarenaSolutions.CCdaToFhirConverter.Extensions;
 using Hl7.Fhir.Model;
@@ -42,7 +44,8 @@ namespace DarenaSolutions.CCdaToFhirConverter
             var medicationStatement = new MedicationStatement
             {
                 Id = id,
-                Subject = new ResourceReference($"urn:uuid:{PatientId}")
+                Subject = new ResourceReference($"urn:uuid:{PatientId}"),
+                Status = MedicationStatement.MedicationStatusCodes.Active
             };
 
             var identifierElements = element.Elements(Defaults.DefaultNs + "id");
@@ -51,91 +54,27 @@ namespace DarenaSolutions.CCdaToFhirConverter
                 medicationStatement.Identifier.Add(identifierElement.ToIdentifier());
             }
 
-            var statusCodeValue = element
-                .Element(Defaults.DefaultNs + "statusCode")?
-                .Attribute("code")?
-                .Value;
-
-            if (!string.IsNullOrWhiteSpace(statusCodeValue))
-            {
-                var statusCode = EnumUtility.ParseLiteral<MedicationStatement.MedicationStatusCodes>(statusCodeValue, true);
-                if (statusCode == null)
-                    throw new InvalidOperationException($"Could not determine medication status code from value: {statusCodeValue}");
-
-                medicationStatement.Status = statusCode;
-            }
-
-            var effectiveTimeElement = element.Element(Defaults.DefaultNs + "effectiveTime");
-            medicationStatement.Effective = effectiveTimeElement?.ToDateTimeElement();
-
-            var doseQuantityElement = element.Element(Defaults.DefaultNs + "doseQuantity");
-            if (doseQuantityElement != null)
-            {
-                Quantity quantity;
-
-                var nullFlavorValue = doseQuantityElement.Attribute("nullFlavor")?.Value;
-                if (!string.IsNullOrWhiteSpace(nullFlavorValue))
-                {
-                    if (!nullFlavorValue.IsValidNullFlavorValue())
-                        throw new InvalidOperationException($"The null flavor value '{nullFlavorValue}' is invalid: {doseQuantityElement}");
-
-                    quantity = new Quantity
-                    {
-                        Extension = new List<Extension>
-                        {
-                            new Extension(Defaults.NullFlavorSystem, new Code(nullFlavorValue))
-                        }
-                    };
-                }
-                else
-                {
-                    var quantityValue = doseQuantityElement.Attribute("value")?.Value;
-                    if (string.IsNullOrWhiteSpace(quantityValue))
-                        throw new InvalidOperationException($"Could not find dosage quantity in: {doseQuantityElement}");
-
-                    quantity = new Quantity
-                    {
-                        Value = decimal.Parse(quantityValue)
-                    };
-
-                    var unitValue = doseQuantityElement.Attribute("unit")?.Value;
-                    if (!string.IsNullOrWhiteSpace(unitValue))
-                        quantity.Unit = unitValue;
-                }
-
-                medicationStatement.Dosage.Add(new Dosage
-                {
-                    DoseAndRate = new List<Dosage.DoseAndRateComponent>
-                    {
-                        new Dosage.DoseAndRateComponent
-                        {
-                            Dose = quantity
-                        }
-                    }
-                });
-            }
-
-            var assignedAuthorElement = element
-                .Element(Defaults.DefaultNs + "author")?
-                .Element(Defaults.DefaultNs + "assignedAuthor");
-
-            if (assignedAuthorElement != null)
-            {
-                var practitionerConverter = new PractitionerConverter(PatientId);
-                var practitioners = practitionerConverter.AddToBundle(
-                    bundle,
-                    new List<XElement> { assignedAuthorElement },
-                    namespaceManager,
-                    cacheManager);
-
-                medicationStatement.InformationSource = new ResourceReference($"urn:uuid:{practitioners[0].Id}");
-            }
+            medicationStatement.Effective = element
+                .Element(Defaults.DefaultNs + "effectiveTime")?
+                .ToDateTimeElement();
 
             bundle.Entry.Add(new Bundle.EntryComponent
             {
                 FullUrl = $"urn:uuid:{id}",
                 Resource = medicationStatement
             });
+
+            var medicationXPath = "n1:consumable/n1:manufacturedProduct/n1:manufacturedMaterial";
+            var medicationEl = element.XPathSelectElement(medicationXPath, namespaceManager);
+            if (medicationEl != null)
+            {
+                var medicationConverter = new MedicationConverter(PatientId);
+                var medication = medicationConverter
+                    .AddToBundle(bundle, new List<XElement> { medicationEl }, namespaceManager, cacheManager)
+                    .First();
+
+                medicationStatement.Medication = new ResourceReference($"urn:uuid:{medication.Id}");
+            }
 
             return medicationStatement;
         }
