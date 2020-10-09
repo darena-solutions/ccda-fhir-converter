f2fff371f4ea29316d1c519efeec2356ca668841
Mahbubul Haque <mhaque@darenasolutions.com>
2020-10-09 14:38:23 -0400

    Organization converter throwing correct exceptions

---
+++ src/DarenaSolutions.CCdaToFhirConverter/Exceptions/ProfileRelatedException.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Xml.Linq;
+using DarenaSolutions.CCdaToFhirConverter.Extensions;
+
+namespace DarenaSolutions.CCdaToFhirConverter.Exceptions
+{
+    /// <summary>
+    /// An exception that is thrown when a profile related issue occurs on a particular element such as the cardinality
+    /// or some invariant related constraint
+    /// </summary>
+    public class ProfileRelatedException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProfileRelatedException"/> class
+        /// </summary>
+        /// <param name="element">The source element the issue occurred on</param>
+        /// <param name="message">The error message</param>
+        /// <param name="additionalXPath">Optionally specify if an additional xpath was applied to <paramref name="element"/>
+        /// to get to the source element</param>
+        public ProfileRelatedException(XElement element, string message, string additionalXPath = null)
+            : base(CreateDefaultMessage(element, message, additionalXPath))
+        {
+            Element = element;
+            AdditionalXPath = additionalXPath;
+        }
+
+        /// <summary>
+        /// Gets the source element the issue occurred on
+        /// </summary>
+        public XElement Element { get; }
+
+        /// <summary>
+        /// Gets an additional xpath that was applied to <see cref="Element"/> to get to the source element
+        /// </summary>
+        public string AdditionalXPath { get; }
+
+        private static string CreateDefaultMessage(
+            XElement element,
+            string message,
+            string additionalXPath)
+        {
+            var fullPath = element.GetAbsolutePath();
+            if (!string.IsNullOrWhiteSpace(additionalXPath))
+                fullPath += additionalXPath.StartsWith("/") ? additionalXPath : $"/{additionalXPath}";
+
+            return $"{message} | Source: {fullPath}";
+        }
+    }
+}
