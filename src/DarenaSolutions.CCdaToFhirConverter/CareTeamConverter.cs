371956a7c1e0f37460fb60acd35ce3785c84e2d5
Mahbubul Haque <mhaque@darenasolutions.com>
2020-10-09 11:43:54 -0400

    Added the care team converter and added initial values

---
+++ src/DarenaSolutions.CCdaToFhirConverter/CareTeamConverter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+using Hl7.Fhir.Model;
+
+namespace DarenaSolutions.CCdaToFhirConverter
+{
+    /// <summary>
+    /// Converter that converts various elements in the CCDA into care team FHIR resources
+    /// </summary>
+    public class CareTeamConverter : BaseConverter
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CareTeamConverter"/> class
+        /// </summary>
+        /// <param name="patientId">The id of the patient referenced in the CCDA</param>
+        public CareTeamConverter(string patientId)
+            : base(patientId)
+        {
+        }
+
+        /// <inheritdoc />
+        protected override IEnumerable<XElement> GetPrimaryElements(XDocument cCda, XmlNamespaceManager namespaceManager)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Resource PerformElementConversion(
+            Bundle bundle,
+            XElement element,
+            XmlNamespaceManager namespaceManager,
+            ConvertedCacheManager cacheManager)
+        {
+            var id = Guid.NewGuid().ToString();
+            var careTeam = new CareTeam
+            {
+                Id = id,
+                Meta = new Meta(),
+                Subject = new ResourceReference($"urn:uuid:{PatientId}")
+            };
+
+            careTeam.Meta.ProfileElement.Add(new Canonical("http://hl7.org/fhir/us/core/StructureDefinition/us-core-careteam"));
+        }
+    }
+}
