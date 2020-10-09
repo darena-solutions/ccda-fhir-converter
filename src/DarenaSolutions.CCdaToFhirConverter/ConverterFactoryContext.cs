cdc4d270cc4652e4b6ea7e4a4c951355941ab786
Mahbubul Haque <mhaque@darenasolutions.com>
2020-10-09 19:55:32 -0400

    Update the converter collection replacer to allow users of the library to inherit from IResourceConverter and perform adding to bundle however they see fit. This means that users of the library should supply their own converter factory method

---
+++ src/DarenaSolutions.CCdaToFhirConverter/ConverterFactoryContext.cs
@@ -0,0 +1,42 @@
+﻿using Hl7.Fhir.Model;
+
+namespace DarenaSolutions.CCdaToFhirConverter
+{
+    /// <summary>
+    /// The context that is passed down to converter factories when registering the converters in the <see cref="CCdaToFhirExecutor"/>.
+    /// This is to provide additional context for creating a converter in a specific manner
+    /// </summary>
+    public class ConverterFactoryContext
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConverterFactoryContext"/> class
+        /// </summary>
+        /// <param name="patientId">The id of the primary patient in the CCDA</param>
+        /// <param name="organizationId">The id of the primary organization in the CCDA</param>
+        /// <param name="bundle">The current bundle that all converters are adding on to</param>
+        public ConverterFactoryContext(
+            string patientId,
+            string organizationId,
+            Bundle bundle)
+        {
+            PatientId = patientId;
+            OrganizationId = organizationId;
+            Bundle = bundle;
+        }
+
+        /// <summary>
+        /// Gets the id of the primary patient in the CCDA
+        /// </summary>
+        public string PatientId { get; }
+
+        /// <summary>
+        /// Gets the id of the primary organization in the CCDA
+        /// </summary>
+        public string OrganizationId { get; }
+
+        /// <summary>
+        /// Gets the current bundle that all converters are adding on to
+        /// </summary>
+        public Bundle Bundle { get; }
+    }
+}
